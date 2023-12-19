0576451c398201512da700ffe730f24354b43ab5
Gauri Naik <80575591+GauriNaik826@users.noreply.github.com>
2023-12-19 09:41:20 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Natural Language Processing", "Deep Learning", "Machine Learning", "Artificial Intelligence"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
